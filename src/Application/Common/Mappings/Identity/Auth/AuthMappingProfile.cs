5ec52d3b180c63d07b1c6c76c2f4d76a70bfcade
Mostafa SAID <mostafa.said7@example.com>
2025-12-30 15:30:41 +0200

    Add Identity mapping profiles to complete organized structure
    - Created AuthMappingProfile for Auth feature mappings
    - Created ProfileMappingProfile for Profile feature mappings
    - Completed organized mappings folder structure
    - All Identity features now fully organized with proper subfolders

---
+++ src/Application/Common/Mappings/Identity/Auth/AuthMappingProfile.cs
@@ -0,0 +1,24 @@
+using AutoMapper;
+using Application.Features.Identity.Auth.DTOs.Requests;
+using Application.Features.Identity.Auth.DTOs.Responses;
+using Domain.Entities.Identity;
+
+namespace Application.Common.Mappings.Identity.Auth
+{
+    public class AuthMappingProfile : Profile
+    {
+        public AuthMappingProfile()
+        {
+            CreateMap<ApplicationUser, AuthResponse>()
+                .ForMember(dest => dest.Token, opt => opt.Ignore())
+                .ForMember(dest => dest.RefreshToken, opt => opt.Ignore())
+                .ForMember(dest => dest.ExpiresAt, opt => opt.Ignore());
+
+            CreateMap<RegisterRequest, ApplicationUser>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore());
+        }
+    }
+}
---
+++ src/Application/Common/Mappings/Identity/Profile/ProfileMappingProfile.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using Application.Features.Identity.Profile.DTOs.Requests;
+using Application.Features.Identity.Profile.DTOs.Responses;
+using Domain.Entities.Identity;
+
+namespace Application.Common.Mappings.Identity.Profile
+{
+    public class ProfileMappingProfile : Profile
+    {
+        public ProfileMappingProfile()
+        {
+            CreateMap<ApplicationUser, UserProfileResponse>()
+                .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}".Trim()));
+
+            CreateMap<UpdateProfileRequest, ApplicationUser>()
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore());
+
+            CreateMap<ApplicationUser, UserPrivacySettings>();
+        }
+    }
+}
